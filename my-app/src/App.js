04bb9cd43108c5cb15eaad92cfd323edc71ae7b8
Oganes <og.marckaryan@yandex.ru>
2025-03-08 19:08:49 +0300

    первые шаги в React

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+      <ul>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
