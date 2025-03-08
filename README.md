33acbe2ef0284e5c5bd917a34220cf33f281447d
Oganes <og.marckaryan@yandex.ru>
2025-03-08 19:42:56 +0300

    остановился на 19 уроке

---
+++ README.md
@@ -0,0 +1,4 @@
+# Изучаю React
+
+Учебник находится по [ссылке](https://code.mu/ru/javascript/framework/react/book/prime/).
+
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,8 @@ import React from "react";
 import "./index.css";
 
 function App() {
-  return (
-    <>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
-      <ul>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-      </ul>
-    </>
-  );
+
+  return 
 }
 
 export default App;
